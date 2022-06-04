2f4ec58bb6589adf7c3c6cb9f1ded6777c803372
RinkM <mikerinkert@gmail.com>
2022-06-04 14:18:15 -0600

    small update to CSS and JS.

--- javascript.js
+++ javascript.js
@@ -10,28 +10,41 @@
 // 2 - scissors
 
 let computerRandChoice = Math.floor(Math.random()*3);
-let humanChoice = prompt("Rock, Paper, or Scissors?  Choose 1,2 or 3.");
-let humanHistory = [];
+let humanChoice = 1;
+
+
 let computerHistory = [];
 let countRock = 0;
 let countPaper = 0;
 let countScissors = 0;
 let humanWins = 0;
 let computerWins = 0;
+let gameOutcomes = [["tie", "paperWin", "rockWin"], ["paperWin", "tie", "scissorsWin"], ["rockWin","scissorsWin", "tie"]];
+
+
+while (humanWins < 3 && computerWins < 3){
+    computerRandChoice = Math.floor(Math.random()*3);
+    computerTurn();
+    humanChoice = prompt("0: Rock\n1: Paper\n2:Scissors\n Enter a number.");
+    decideWinner();
+}
 
 
 // tracks human plays. adds one to correct counter. 
 // need to make the choices into 0 ,1,2
 function humanTracker(choice){
-    if(choice == 1){
+    if(choice == 0){
         countRock++
+    } else if(choice == 1){
+        countPaper++
     } else if(choice == 2){
-        countPaper ++
-    } else if(choice == 3){
-        countScissors ++
+        countScissors++
     }
 };
 
+function computerTracker(){
+    
+}
 
 // computer looks at counts. checks which is biggest. if none, random number between the ties.
 // can i use an array to track this.  then just find max? if no max, return random. 
@@ -56,13 +69,14 @@ function gameRound(){
 //do i have to pass h.choice c.choice?  once they update else where..
 //what does this return?
 function decideWinner(humanChoice,computerChoice){
-    gameOutcomes[humanChoice][computerChoice]
+    
+    gameOutcomes[parseInt(humanChoice)][computerChoice]
 }
 
 
 // two options : 012 ;
 
-let gameOutcomes = [["tie", "paperWin", "rockWin"], ["paperWin", "tie", "scissorsWin"], ["rockWin","scissorsWin", "tie"]];
+
 
 //what's the loop? track wins.   plays text.
 while (computerWins <= 3 || humanWins <= 3){
