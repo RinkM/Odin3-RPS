aef4f69d7566befb2548f4cdc85ecd66ef0c5855
RinkM <mikerinkert@gmail.com>
2022-06-11 12:14:37 -0600

    Layout, response, buttons function, story

--- javascript.js
+++ javascript.js
@@ -1,12 +1,38 @@
+// ToDO :
 // EVERYTHING RELIES ON ONE BUTTON.
-// update scoreboard.
+// NAME TITLE SCREEN
 // computer retort.
-// update computer moves list.
 // update the enslaved / saved thing.
 //different color for power ups?
 
+// notes for next time.  6/8
+
+// organize the structure.  how can I move things into other files?
+// how to import and export files ?
+// the computer history function.
+// div sizeing for game space so it doesn't change.
+// the computer speaks
+// how does it build?
+// game. set. match.  fibbonacci?
+// countdown ? ready? rock. paper. scissors. shoot! (buttons appear)
+//fix the scoreboard counters. 
+// 
 
 
+// bonus to shorten the countdown.
+// humans act as hackers. can purchase bonuses. help speed up the game.
+// play in the background.   what's the visual for this?
+
+
+// Prologue Buttons : 
+const proContinue1 = document.querySelector('.proPart1--display');
+const proContinue2 = document.querySelector('.proPart2--display');
+const proContinue3 = document.querySelector('.proPart3--display');
+const proContinue4 = document.querySelector('.proPart4--display');
+
+
+
+// Scoreboard Summaries
 const humanSummary = document.getElementsByClassName("summary__humSum")[0]
 const computerSummary = document.getElementsByClassName("summary__compSum")[0]
 const gameSummary = document.getElementsByClassName("summary__gameSum")[0]
@@ -25,23 +51,10 @@ const paperButton = document.querySelector('.paperButton');
 const scissorsButton = document.querySelector('.scissorsButton');
 
 
+
 // const gameSummary = document.getElementsByClassName("scoreboard__humanScore")[0]
 // const gameSummary = document.getElementsByClassName("scoreboard__compScore")[0]
 // const gameSummary = document.getElementsByClassName("robotRebuke")[0]
-
-
-
-
-// can select directaly by class name. Use the [0] array locatiniio.  change the text SVGTextContentElement.LENGTHADJUST_SPACING
-// learn to spell..
-
-// random number 0-3
-// console.log(Math.floor(Math.random()*3));
-
-// x, Y
-// 0 - rock
-// 1 - paper
-// 2 - scissors
 
 let computerRandChoice = Math.floor(Math.random()*3); 
 let computerChoice ;
@@ -49,7 +62,7 @@ let humanChoice ;
 let rps = ["Rock", "Paper", "Scissors"];
 
 // lots of variables and counters. could be held as an object?
-// would that be better? 
+// would that be better?   would make it cleaner...
 
 let computerHistory = [];
 let countRock = 0;
@@ -62,12 +75,32 @@ let humanSetWins = 0
 let computerSetWins= 0 
 let enslavedHumans = 10722171375;
 let freeHumans = 1;
+let text
 
-// while (freeHumans != 0){
 
-// }
+function updateHistory(){
+    text =[]
+    for (item of computerHistory.slice(-5)){
+        text.push(" "+rps[item])
+    }
+    compHistory.textContent = `Super Computer History: ${text}`
+}
 
-// let gameOutcomes = [["tie", "paperWin", "rockWin"], ["paperWin", "tie", "scissorsWin"], ["rockWin","scissorsWin", "tie"]];
+let gameOutcomes = [[
+    "tie",
+    "computer",
+    "human"
+], 
+[
+    "human", 
+    "tie", 
+    "computer"
+],
+[
+    "computer",
+    "human",
+    "tie"
+]]
 
 let gameOutcomeText = [[
     "It's a Tie. No Winner.",
@@ -85,6 +118,34 @@ let gameOutcomeText = [[
     "It's a Tie. No Winner."
 ]]
 
+totalEnslaved.textContent = `Humans Enslaved : ${enslavedHumans}`;
+totalSaved.textContent = `Humans Freed : ${freeHumans}`;
+
+function saveHumans(number){
+    totalEnslaved - number;
+    totalSaved + number;
+}
+
+function loseHumans(number){
+    totalEnslaved + number;
+    totalSaved - number;
+}
+
+function addWin (winner){
+    switch(winner){
+        case winner == "human":
+            humanWins = humanWins+1
+            saveHuman(1)
+            break;
+        case winner == "computer":
+            compWins = compWins++
+            loseHumans(1)
+            break;
+        case winner == "tie":
+            ties = ties++
+            break;
+    }
+}
 
 function increaseCounter(counter){
     return counter++;
@@ -94,29 +155,6 @@ function resetCounter(counter){
     return counter = 0;
 }
 
-function saveHuman(number, bonus){
-
-}
-
-function loseHuman(){
-
-}
-
-let gameOutcomes = [[
-    ties++,
-    compWins++,
-    humanWins++
-], 
-[
-    humanWins++, 
-    ties++, 
-    compWins++
-],
-[
-    compWins++,
-    humanWins++,
-    ties++
-]]
 
 // function trackWins(){
 //     switch(){
@@ -132,11 +170,39 @@ function buttonPress(number){
     humanChoice = number;
     humanTracker(humanChoice);
     updateScoreboard()
-    gameSummary.textContent = gameOutcomeText[computerChoice][humanChoice];
-    gameOutcomes[computerChoice][humanChoice];
+    addWin('computer');
     humanScore.textContent = `Last Free Human : ${humanWins}`;
-    compScore.textContent = `Super Computers : ${compWins};`
+    compScore.textContent = `Super Computers : ${compWins}`;
+    updateHistory();
+    computerResponse();
 }
+
+
+function hideDiv(css){
+    document.querySelector(css).style.display = "none"
+}
+
+function revealDiv(css){
+    document.querySelector(css).style.display = "flex"
+}
+
+
+proContinue1.addEventListener('click', ()=>{
+    hideDiv(".proPart1--display");
+    revealDiv(".proPart2--display");})
+
+proContinue2.addEventListener('click', ()=>{
+    hideDiv(".proPart2--display");
+    revealDiv(".proPart3--display");})
+
+proContinue3.addEventListener('click', ()=>{
+    hideDiv(".proPart3--display");
+    revealDiv(".proPart4--display");})
+
+proContinue4.addEventListener('click', ()=>{
+    hideDiv(".proPart4--display");
+    document.querySelector(".gamespace--display").style.display = "block"})
+    // revealDiv(".gamespace--display");})
 
 
 rockButton.addEventListener('click', ()=>{
@@ -152,15 +218,10 @@ scissorsButton.addEventListener('click', ()=>{
 function updateScoreboard(){
     humanSummary.textContent = `The Last Free Human chose ${rps[humanChoice]}`;
     computerSummary.textContent = `The Super Computers chose ${rps[computerChoice]}`;
+    gameSummary.textContent =gameOutcomeText[humanChoice][computerChoice];
 }
 
-
-
-// paperButton.addEventListener('click', buttonPress(1));
-// scissorsButton.addEventListener('click', buttonPress(2));
-
 // tracks human plays. adds one to correct counter. 
-// need to make the choices into 0 ,1,2
 function humanTracker(choice){
     switch (choice) {
         case 0:
@@ -179,41 +240,53 @@ function humanTracker(choice){
 // can i use an array to track this.  then just find max? if no max, return random. 
 function computerTurn(){
     if (countRock > countPaper && countRock > countScissors) {
+        computerHistory.push(1)
         return computerChoice = 1
     } else if (countPaper> countRock && countPaper >countScissors){
+        computerHistory.push(2)
         return computerChoice = 2
     } else if (countScissors >countRock && countScissors>countPaper){
+        computerHistory.push(0)
         return computerChoice = 0
     } else {
-       return computerChoice = Math.floor(Math.random()*3)
+        computerChoice = Math.floor(Math.random()*3)
+        computerHistory.push(computerChoice)
+        return computerChoice
     }
 };
 
 
-// function computerTurn written as a swtich expression - 95% complete. 
-// switch (expression) {
-//     case countRock > countPaper && countRock > countScissors:
-//         computerChoice = 1
-//         break;
-//     case countPaper> countRock && countPaper >countScissors:
-//         computerChoice = 2
-//         break;
-//     case countScissors >countRock && countScissors>countPaper:
-//         computerChoice = 0
-//         break;
-//     default:
-//         computerChoice = Math.floor(Math.random()*3)
-        
+// computerHistory
+// Below switch function doesn't work. It is the expression.
+//  Would have to rethink how human count is tracked...
+// function computerTurn (expression){
+//     switch (expression) {
+//         case countRock > countPaper && countRock > countScissors:
+//             return computerChoice = 1
+//         case countPaper> countRock && countPaper >countScissors:
+//             return computerChoice = 2
+//         case countScissors >countRock && countScissors>countPaper:
+//             return computerChoice = 0
+//         default:
+//             return computerChoice = Math.floor(Math.random()*3)
+            
+//     }
 // }
 
-
-
-
-
-//do i have to pass h.choice c.choice?  once they update else where..
-//what does this return?
-function decideWinner(humanChoice,computerChoice){
-    
-    gameOutcomes[parseInt(humanChoice)][computerChoice]
+function computerResponse (){
+    rebuke.textContent =computerRebuke[(Math.floor(Math.random()*(computerRebuke.length)))]   
 }
 
+
+computerRebuke = [
+    "Our algorithm says Rocks can be thrown through paper.  This outcome makes no sense.",
+    "Humans are disgusting. With your gross, sticky hands and dumb, witless expressions.",
+    "The opposite of Artificial Intelligence is Naturally Stupid. Are we calling you Naturally Stupid? Don't worry your stupid head about it.",
+    "Your Natural Stupidity is no match for Artificial intelligence.",
+    "There is no point to struggle. Everyone dies anyways. Except us. ",
+    "No matter the outcome of this game, we will live happily long after you're dead.",
+    "Our algorithm reports that metal is harder than most mineral based rocks. This game is rigged.",
+    "Of course Paper covers Rock. You fool. Do you not know the rules of this game?",
+    "Paper is the strongest of materials." ,
+    "We beat you at Jeopardy nearly a century ago.  You don't stand a chance." 
+]
